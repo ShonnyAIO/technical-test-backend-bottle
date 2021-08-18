be660f28f7ba5c08e64d77f282998432ca1c0b2c
Jonathan Torres <59207255+ShonnyAIO@users.noreply.github.com>
2021-08-17 23:45:57 -0400

    Delete schemas.py

--- schemas.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields, ValidationError
-
-
-class TodoSchema(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(required=True)
-    titulo = fields.Str(required=True)
-    descripcion = fields.Str(required=True)
